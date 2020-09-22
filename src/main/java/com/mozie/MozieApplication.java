060c4201bedd04c30999e9932af772380940c77f
Vajna Tímea Dominika <vajnatimi@gmail.com>
2020-09-22 11:29:22 +0200

    initial commit

---
+++ src/main/java/com/mozie/MozieApplication.java
@@ -0,0 +1,13 @@
+package com.mozie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MozieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mozie/MozieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
