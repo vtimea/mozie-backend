dfd88de77b5d8a9a51f48eed69ba5b10f2843711
Vajna Tímea <vajnatimi@gmail.com>
2020-11-16 15:05:21 +0100

    simplified code

--- src/main/java/com/mozie/controller/TicketController.java
+++ src/main/java/com/mozie/controller/TicketController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8081")
@@ -28,12 +27,8 @@ public class TicketController {
         List<Ticket> tickets;
         if (type == null || type.isEmpty()) {
             tickets = ticketService.getAllTicketTypes();
-        } else if (type.toLowerCase().contains("2d")) {
-            tickets = ticketService.getTicketTypeByType("2d");
-        } else if (type.toLowerCase().contains("3d")) {
-            tickets = ticketService.getTicketTypeByType("3d");
         } else {
-            tickets = new ArrayList<>();
+            tickets = ticketService.getTicketTypeByType(type);
         }
         List<TicketDto> ticketDtos = DtoConverters.convertToTicketDtoList(tickets);
         return new ResponseEntity<>(ticketDtos, HttpStatus.OK);
@@ -41,12 +36,9 @@ public class TicketController {
 
     @PostMapping("payments/ticket")
     public ResponseEntity<ClientTokenResponse> generateClientToken(@RequestBody TicketOrder ticketOrder) {
-        // 1. Create transaction
         Transaction transaction = ticketService.createTransaction(ticketOrder);
         ticketService.saveTransaction(transaction);
-        // 2. Generate client token
         String clientToken = ticketService.createClientToken();
-        // 3. Return client token
         ClientTokenResponse response = new ClientTokenResponse();
         response.setClientToken(clientToken);
         return new ResponseEntity<>(response, HttpStatus.OK);
